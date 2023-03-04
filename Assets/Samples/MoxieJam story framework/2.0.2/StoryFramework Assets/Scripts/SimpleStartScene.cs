ac6066b3b6044b1e7effbbbc0ddca9fdb2b4976c
LisaMyhrman <lisa@myhrman.me>
2023-03-04 11:30:34 +0100

    Framework added

---
+++ Assets/Samples/MoxieJam story framework/2.0.2/StoryFramework Assets/Scripts/SimpleStartScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using StoryFramework;
+
+public class SimpleStartScene : MonoBehaviour
+{
+    [SerializeField]
+    SceneRef firstInGameScene;
+
+    [SerializeField]
+    GameObject inGameUiPrefab;
+
+    public void StartGame()
+    {
+        Game.StartNewGame(firstInGameScene);
+    }
+
+    public void QuitGame()
+    {
+        Game.Quit();
+    }
+}
